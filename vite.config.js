84f379d36698ca386f84dcba77c2a6885a0b0dff
kaishi <arraffiardhiansyah@gmail.com>
2026-09-13 10:09:08 +0700

    terakhir plissss

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   base: process.env.VITE_BASE_PATH || '/',
   plugins: [react(), tailwindcss()],
-  base: "/arraffi0"
+  base: "/Arraffi0"
 })
